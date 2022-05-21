be1f9823fab852c5b0385b4a7b2ec516db928440
Le Tuan Anh <letuananh.nuclphys@gmail.com>
2022-05-21 12:58:56 +0700

    add more physics process im physicslist

--- src/physicslist.cc
+++ src/physicslist.cc
@@ -1,9 +1,28 @@
 #include "physicslist.hh"
 #include "G4RadioactiveDecayPhysics.hh"
+#include "RadioactiveDecayPhysics.hh"
+#include "G4EmStandardPhysics.hh"
+#include "G4StoppingPhysics.hh"
+#include "G4IonPhysicsXS.hh"
+#include "G4IonElasticPhysics.hh"
+#include "G4HadronPhysicsQGSP_BIC_HP.hh"
 physicslist::physicslist(/* args */)
 : G4VModularPhysicsList()
 {
-
+    // Hadron Inelastic physics
+     RegisterPhysics( new G4HadronPhysicsQGSP_BIC_HP());
+    ////RegisterPhysics( new G4HadronInelasticQBBC());        
+    ////RegisterPhysics( new G4HadronPhysicsINCLXX());
+    ////RegisterPhysics( new G4HadronPhysicsFTFP_BERT_HP());
+    // Ion elastic
+    RegisterPhysics(new G4IonElasticPhysics());
+    //Ion inelastic
+    RegisterPhysics(new G4IonPhysicsXS());
+    //Stopping particle
+    RegisterPhysics(new G4StoppingPhysics());
+    // EM physics
+    RegisterPhysics(new G4EmStandardPhysics());
     // Radioractive decay
-    RegisterPhysics(new G4RadioactiveDecayPhysics());
+    //RegisterPhysics(new G4RadioactiveDecayPhysics());
+    RegisterPhysics(new RadioactiveDecayPhysics());
 }
